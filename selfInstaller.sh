e794eade709b696e33eeac201dc8023c52a04491
FSCHMID-VC <florian.schmid@vision-components.com>
2025-11-24 14:05:42 +0100

    Update installation command in selfInstaller.sh

--- selfInstaller.sh
+++ selfInstaller.sh
@@ -9,9 +9,7 @@ if [ -d platform-builder ]; then
     sudo chmod 755 -R /opt/platform-builder/
     scp -r  platform-builder/app_platform/license /opt/platform-builder/ 
   fi
-  cd platform-builder  && bash install.sh
-
-#   cd platform-builder && git reset --hard HEAD && git clean -fd && git pull && bash install.sh
+  cd platform-builder && git reset --hard HEAD && git clean -fd && git pull && bash install.sh
 else 
   git clone https://github.com/Notavis-GmbH/platform-builder && cd platform-builder && bash install.sh
 fi
