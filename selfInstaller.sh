89f256eb6ab17b2dfcbe39c186dc4ee9801c7d07
FSCHMID-VC <florian.schmid@vision-components.com>
2025-11-24 14:33:22 +0100

    Update selfInstaller.sh for user and directory permissions
    Refactor selfInstaller.sh to use current user and improve directory handling.

--- selfInstaller.sh
+++ selfInstaller.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-
-
-cd /home/raspberrypi/
+user=$(whoami)
+cd  ~
 if [ -d platform-builder ]; then 
-  if [ -f platform-builder/app_platform/license ]; then 
-    sudo mkdir -p /opt/platform-builder/
-    sudo chown pi:pi /opt/platform-builder/
-    sudo chmod 755 /opt/platform-builder/
+  if [ -d platform-builder/app_platform/license ]; then 
+    sudo mkdir -p /opt/platform-builder/license
+    sudo chown $user:$user -R /opt/platform-builder/
+    sudo chmod 755 -R /opt/platform-builder/
     scp -r  platform-builder/app_platform/license /opt/platform-builder/ 
   fi
-  cd platform-builder  && bash install.sh
-
-cd platform-builder && git reset --hard HEAD && git clean -fd && git switch feat/siemens2 && git pull && bash install.sh
+    cd platform-builder && git reset --hard HEAD && git clean -fd && git switch feat/siemens2 &&  git pull && bash install.sh
 else 
   git clone https://github.com/Notavis-GmbH/platform-builder && cd platform-builder && bash install.sh
 fi
