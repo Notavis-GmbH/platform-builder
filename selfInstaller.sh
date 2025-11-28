0cfbc9ac2a9437bf9fcf8dd0e0c5c06c99b50769
FSCHMID-VC <florian.schmid@vision-components.com>
2025-11-28 11:57:18 +0100

    Switch git branch from feat/siemens2 to master

--- selfInstaller.sh
+++ selfInstaller.sh
@@ -8,7 +8,7 @@ if [ -d platform-builder ]; then
     sudo chmod 755 -R /opt/platform-builder/
     scp -r  platform-builder/app_platform/license /opt/platform-builder/ 
   fi
-    cd platform-builder && git reset --hard HEAD && git clean -fd && git switch feat/siemens2 &&  git pull && bash install.sh
+    cd platform-builder && git reset --hard HEAD && git clean -fd && git switch master &&  git pull && bash install.sh
 else 
   git clone https://github.com/Notavis-GmbH/platform-builder && cd platform-builder && bash install.sh
 fi
